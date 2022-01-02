2ec72de74205c7edb0a8a85d5e696e5e4c79b612
vmehrotra50 <33035462+vmehrotra50@users.noreply.github.com>
2022-01-02 14:31:33 -0500

    Create 88: Merge Sorted Array.py

---
+++ 88: Merge Sorted Array.py
@@ -0,0 +1,6 @@
+# This optimal solution is similar to how the merge feature of merge sort works. For those unfamiliar to merge sort, it's a divide-and-conquer sorting
+# algorithm that involves repeatedly diving the array into halfs until you have enough subarrays, then you merge those sorted subarrays together to create
+# a new sorted subarray, and this merging process continues until the original array is reconstructed in sorted order. The division step takes O(log(n))
+# and the merge step takes O(n) times. As a result, the solution to this problem will take O(n) time (in this case, it would be O(n+m) time for each array).
+
+
